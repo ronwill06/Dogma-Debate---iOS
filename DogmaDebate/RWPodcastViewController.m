1816cab8d73b84118e10bb3645a67a75c875463a
ronwill06 <ronwill06@gmail.com>
2015-07-22 08:31:36 -0400

    Audio Queue added

--- DogmaDebate/RWPodcastViewController.m
+++ DogmaDebate/RWPodcastViewController.m
@@ -6,7 +6,24 @@
 //  Copyright (c) 2015 RondaleWilliams. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
 #import "RWPodcastViewController.h"
+
+
+static const int kNumberBuffers = 3;
+
+struct AQPlayerState {
+    AudioStreamBasicDescription   mDataFormat;
+    AudioQueueRef                 mQueue;
+    AudioQueueBufferRef           mBuffers[kNumberBuffers];
+    AudioFileID                   mAudioFile;
+    UInt32                        bufferByteSize;
+    SInt64                        mCurrentPacket;
+    UInt32                        mNumPacketsToRead;
+    AudioStreamPacketDescription  *mPacketDescs;
+    bool                          mIsRunning;
+};
 
 @interface RWPodcastViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *epsisodeLabel;
@@ -29,6 +46,9 @@
 }
 
 - (IBAction)playAudio:(id)sender {
+    
+    //AVPlayer *player;
+    
 }
 
 - (IBAction)rewindAudio:(id)sender {
