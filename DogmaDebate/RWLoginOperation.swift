45454f7ee21dac4c10b8da6bef2ae96b31996186
Rondale Williams <ronwill06@gmail.com>
2016-02-01 22:02:26 -0500

    Added OAuth work

--- DogmaDebate/RWLoginOperation.swift
+++ DogmaDebate/RWLoginOperation.swift
@@ -7,8 +7,12 @@
 //
 
 import Foundation
+import OAuthSwift
 
 class RWLoginOperation {
+    
+    let clientId = "xqBztkf8SReYxRd0dz0wlOVb4wO8Lm"
+    let clientSecret = "4jNGTh18pT0ESyv5zJEzf0nabsH0At"
     
     
     init() {
@@ -17,41 +21,30 @@ class RWLoginOperation {
     
     
     convenience init(username: String, password: String) {
-        
-        let authString = NSString(format:"%@:%@", username, password)
-        let data = authString.dataUsingEncoding(NSUTF8StringEncoding)
-        let encoding = data?.base64EncodedDataWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
-        
-        if let encoding = encoding {
-            let authHeader = NSString(format:"Basic %@", encoding)
-            
-            let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-            configuration.HTTPAdditionalHeaders =  [
-                "Content-Type" : "application/rss+xml",
-                "Authorization" : authHeader
-            ]
-        }
-        
-        let url = NSURL(string:"login.dogmadebate.com/feed/?oauth=token")
-        
-        if let url =  url {
-            _ = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-                
-            if (error != nil) {
-                    return;
-                } else {
-                    
-                    let httpResponse = response as? NSHTTPURLResponse
-                    if httpResponse?.statusCode == 200 {
-                        
-                    }
-                    
-                }
-                
-            }).resume()
-        }
-
         self.init()
+        
+        let oauthSwift = OAuth2Swift(
+            consumerKey: clientId,
+            consumerSecret: clientSecret,
+            authorizeUrl: "http://login.dogmadebate.com/feed/oauth/authorize",
+            accessTokenUrl: "http://login.dogmadebate.com/feed/oauth/access_token",
+            responseType: "code")
+        
+        if let url = NSURL(string: "login.dogmadebate.com/oauth/authorize?response_type=code&amp;client_id=\(clientId)") {
+            
+            oauthSwift.authorizeWithCallbackURL(url, scope:"login.dogmadebate.com/feed/", state: "", success: { (credential, response, parameters) -> Void in
+               print("Response:\(response)")
+                
+                }) { (error) -> Void in
+                 print("Error:\(error.localizedDescription)")
+            }
+        }
+        
+        
+        
     }
+    
+    
+    
 
 }
