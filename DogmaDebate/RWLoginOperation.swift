45133c84ca8f1678c4c1f1da6eeacb2e5bc46dc2
Rondale Williams <ronwill06@gmail.com>
2016-02-10 10:13:24 -0500

    OAuth fully implemented

--- DogmaDebate/RWLoginOperation.swift
+++ DogmaDebate/RWLoginOperation.swift
@@ -30,25 +30,32 @@ class RWLoginOperation {
         authorizeUrl:"http://login.dogmadebate.com/oauth/authorize" ,
         accessTokenUrl:  "http://login.dogmadebate.com/oauth/token",
         responseType: "token",
-        contentType: "application/json")
+        contentType: "application/x-www-form-urlencoded")
         
         oauthSwift.accessTokenBasicAuthentification = true
         
-        oauthSwift.client.post("http://login.dogmadebate.com/feed/?oauth=token", parameters: ["grant_type" : password], headers: ["Authorization" : "basic" ], success: { (data, response) -> Void in
+        oauthSwift.client.post("http://login.dogmadebate.com/?oauth=token&grant_type=password", parameters: ["client_id" : clientId ,"client_secret" : clientSecret,"grant_type" : "password", "username" : username, "password" : password], headers: ["Authorization" : "basic"], success: { (data, response) -> Void in
             print("\(response)")
             
-            do {
-                let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as? [String : AnyObject]
-             print("\(json)")
-                
-            } catch {
-                
+            if response.statusCode == 200 {
+                let dataString = NSString(data: data, encoding:NSUTF8StringEncoding)
+                print("\(dataString)")
             }
             
             }) { (error) -> Void in
                 print("\(error.localizedDescription)")
+                let alertView = UIAlertView()
+                alertView.title = "Incorrect Password"
+                alertView.message = "We could not log you in. Please try again."
+                alertView.addButtonWithTitle("Dismiss")
+                alertView.cancelButtonIndex = 0
+                alertView.show()
+                
+            
         }
-
+        
+        
+        
     }
     
     
